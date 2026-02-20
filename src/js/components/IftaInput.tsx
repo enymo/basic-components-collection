79ba4e8061fa9271781d919ed9a61083a34ec11f
mimibun <166717259+mimibun@users.noreply.github.com>
2026-02-20 23:11:45 +0100

    feat: add chevron and chevron animation

--- src/js/components/IftaInput.tsx
+++ src/js/components/IftaInput.tsx
@@ -21,14 +21,21 @@ export default function IftaInput({
     const ref = useRef<GlissadeInputRef>(null);
 
     const handleClick = useCallback(() => {
-        ref.current?.focus();
+        if (ref.current !== null) {
+            if (ref.current instanceof HTMLSelectElement) {
+                ref.current.showPicker();
+            }
+            else {
+                ref.current.focus();
+            }
+        }
     }, [ref])
 
     return (
         <div className="flex flex-col gap-2">
             <div onClick={handleClick} className={clsx(
-                "flex flex-col justify-between h-(--input-ifta-height)",
-                "border-(length:--input-border-width) rounded-xl cursor-text placeholder:text-text-100",
+                "relative flex flex-col justify-between h-(--input-ifta-height)",
+                "border-(length:--input-border-width) rounded-xl cursor-text placeholder:text-text-100 group",
                 {
                     "bg-bg-100 border-neutral-300 hover:border-neutral-400 focus-within:shadow-primary-300 focus-within:hover:shadow-primary-300 focus-within:border-primary-500 focus-within:hover:border-primary-500": !disabled && error === undefined,
                     "bg-bg-400 border-neutral-300": disabled,
@@ -37,14 +44,16 @@ export default function IftaInput({
                 "shadow-input shadow-neutral-300 w-100"
             )}>
                 <label htmlFor={id} className={clsx(
-                    "body-xs-md cursor-text",
+                    "body-xs-md cursor-text px-(--input-ifta-padding-horizontal) pt-1.5",
                     {
                         "text-text-700": !disabled,
                         "text-text-500": disabled,
                     }
                 )}>{label}</label>
-                <GlissadeInput ref={ref} {...props} disabled={disabled} id={id} className="outline-none body-m text-text-900 disabled:text-text-500 " />
-                {props.type === "select" && <Chevron />}
+                <GlissadeInput ref={ref} {...props} disabled={disabled} id={id} className="px-(--input-ifta-padding-horizontal) pb-1.5 outline-none body-m text-text-900 disabled:text-text-500 appearance-none peer" />
+                {props.type === "select" && <Chevron className={clsx(
+                    "absolute w-4 fill-neutral-700 top-1/2 -translate-y-1/2 right-(--input-ifta-padding-horizontal) peer-open:rotate-180 transition duration-200 ease-in-out"
+                )} />}
             </div>
             {error && <Error>{error}</Error>}
         </div>
