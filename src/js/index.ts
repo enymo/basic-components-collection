c06945ee5a7d661eb1a7c7158a476baf5db2b6d0
Gideon Ritter <gideon.ritter@enymo.com>
2026-02-24 11:53:06 +0100

    Update exports

--- src/js/index.ts
+++ src/js/index.ts
@@ -3,7 +3,9 @@ export { default as Checkbox } from "./components/Checkbox";
 export { default as CheckboxInput } from "./components/CheckboxInput";
 export { default as Error } from "./components/Error";
 export { default as IftaInput } from "./components/IftaInput";
+export { default as Input } from "./components/Input";
 export { default as RadioButton } from "./components/RadioButton";
 export { default as RadioInput } from "./components/RadioInput";
 export { default as Tabs } from "./components/Tabs";
 export * from "./icons";
+
