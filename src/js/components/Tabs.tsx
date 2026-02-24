422c5f13c2025065b00122ab9876c1eb49537d12
mimibun <166717259+mimibun@users.noreply.github.com>
2026-02-24 09:46:32 +0100

    Update src/js/components/Tabs.tsx
    Co-authored-by: G-Ritter <28601586+G-Ritter@users.noreply.github.com>

--- src/js/components/Tabs.tsx
+++ src/js/components/Tabs.tsx
@@ -18,8 +18,8 @@ export default function Tabs<T extends string>({
             {tabs.map((tab) => (
                 <button type="button" onClick={() => onChange(tab.value)} className={clsx("box-border border-b-2 border-transparent px-2 pt-2.5 pb-2 body-m-md translate-y-px",
                     {
-                        "text-text-500 translate-y-px hover:text-text-700": tab.value !== value,
-                        "border-b-2 border-b-primary-500 text-primary-500 translate-y-px": tab.value === value
+                        "text-text-500 hover:text-text-700": tab.value !== value,
+                        "border-b-primary-500 text-primary-500": tab.value === value
                     }
                 )}>{tab.label}</button>
             ))}
