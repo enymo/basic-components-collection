9ff51c3ebb2af18668aedca35d642ff335c2914d
mimibun <166717259+mimibun@users.noreply.github.com>
2026-02-24 09:46:26 +0100

    Update src/js/components/Tabs.tsx
    Co-authored-by: G-Ritter <28601586+G-Ritter@users.noreply.github.com>

--- src/js/components/Tabs.tsx
+++ src/js/components/Tabs.tsx
@@ -16,7 +16,7 @@ export default function Tabs<T extends string>({
     return (
         <nav className="flex flex-row gap-1.5 border-b border-b-neutral-200">
             {tabs.map((tab) => (
-                <button onClick={() => onChange(tab.value)} className={clsx("box-border border-b-2 border-transparent px-2 pt-2.5 pb-2 body-m-md translate-y-px",
+                <button type="button" onClick={() => onChange(tab.value)} className={clsx("box-border border-b-2 border-transparent px-2 pt-2.5 pb-2 body-m-md translate-y-px",
                     {
                         "text-text-500 translate-y-px hover:text-text-700": tab.value !== value,
                         "border-b-2 border-b-primary-500 text-primary-500 translate-y-px": tab.value === value
