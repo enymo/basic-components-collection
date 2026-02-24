a037e373a589cde4070ec8fcb389a7cad0e6fb27
Gideon Ritter <gideon.ritter@enymo.com>
2026-02-24 12:02:57 +0100

    Revert "Allow default icons to be used"
    This reverts commit 6b41068c7cec235698378624ed9dfd052f977ddd.

--- src/js/storybook.ts
+++ src/js/storybook.ts
@@ -1,6 +1,17 @@
 import "../css/storybook.css";
-import configureDefaultIcons from "./defaultIcons";
+import CheckIcon from "../svg/check-solid-full.svg?react";
+import ChevronDown from "../svg/chevron-down-solid-full.svg?react";
+import CircleExclamationIcon from "../svg/circle-exclamation-solid-full.svg?react";
+import CircleNotchIcon from "../svg/circle-notch-solid-full.svg?react";
+import CircleQuestionIcon from "../svg/circle-question-regular-full.svg?react";
+import { configureIcons } from "./icons";
 
-configureDefaultIcons();
+configureIcons({
+    chevron: ChevronDown,
+    tooltip: CircleQuestionIcon,
+    error: CircleExclamationIcon,
+    spinner: CircleNotchIcon,
+    check: CheckIcon
+})
 
 export const sleep = (duration: number) => new Promise<void>(resolve => setTimeout(resolve, duration));
