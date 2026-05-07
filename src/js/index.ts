e1ab2983bb5c82bb209ba4ad0a82a65373681077
Gideon Ritter <gideon.ritter@enymo.com>
2026-05-07 09:29:39 +0200

    Fix export

--- src/js/index.ts
+++ src/js/index.ts
@@ -3,7 +3,7 @@ export { default as Checkbox } from "./components/Checkbox";
 export { default as CheckboxInput } from "./components/CheckboxInput";
 export { default as Error } from "./components/Error";
 export { default as IftaInput } from "./components/IftaInput";
-export { default as Input } from "./components/Input";
+export { default as Input, type InputProps } from "./components/Input";
 export { default as InputFrame } from "./components/InputFrame";
 export { default as RadioButton } from "./components/RadioButton";
 export { default as RadioInput } from "./components/RadioInput";
