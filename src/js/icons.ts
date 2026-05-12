09c422f4d73df25f531bbe213bf129212670d6a4
Gideon Ritter <gideon.ritter@enymo.com>
2026-05-12 14:35:27 +0200

    Fix icon adapter error message

--- src/js/icons.ts
+++ src/js/icons.ts
@@ -1,12 +1,17 @@
 import type { FC, SVGProps } from "react";
 
+const missingIcons = () => {
+    throw new Error(`The icons for the basic component collection have not been configured! Please configure the icons by calling 'configureIcons' when booting your application`);
+}
+
 export type Icon = FC<SVGProps<SVGSVGElement>>;
 
-export let Chevron: Icon;
-export let Tooltip: Icon;
-export let Error: Icon;
-export let Spinner: Icon;
-export let Check: Icon;
+export let Chevron: Icon = missingIcons;
+export let Tooltip: Icon = missingIcons;
+let ErrorIcon: Icon = missingIcons;
+export { ErrorIcon as Error };
+export let Spinner: Icon = missingIcons;
+export let Check: Icon = missingIcons;
 
 type IconName = "chevron" | "tooltip" | "error" | "spinner" | "check";
 export function configureIcons({
@@ -18,7 +23,7 @@ export function configureIcons({
 }: Record<IconName, Icon>) {
     Chevron = chevron;
     Tooltip = tooltip;
-    Error = error;
+    ErrorIcon = error;
     Spinner = spinner;
     Check = check;
 }
